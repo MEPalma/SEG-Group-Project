3c5f17938616370d1ccd21c5bf1556571d9b41ee
mep2g17 <mep2g17@soton.ac.uk>
2019-02-20 14:46:07 +0000

    __INIT__

---
+++ Project/SEG/src/Main/java.java
@@ -0,0 +1,14 @@
+package Main;
+
+/**
+ *
+ * @author Group 31
+ */
+public class java {
+
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}

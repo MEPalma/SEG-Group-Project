dcc98968670c592bc0d6a8751ed196ce4f3750c8
mep2g17 <mep2g17@soton.ac.uk>
2019-02-22 07:51:30 +0000

    I have found some more performance in the parser, 5 seconds

--- Project/SEG/src/DatabaseManager/DataExchange.java
+++ Project/SEG/src/DatabaseManager/DataExchange.java
@@ -1,17 +1,15 @@
 package DatabaseManager;
 
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.NumberFormat;
 import java.util.*;
 
 /*
  * Created by Marco-Edoardo Palma.
  */
-
 import Commons.*;
-import java.time.chrono.ThaiBuddhistEra;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,6 +19,7 @@ import java.util.logging.Logger;
  */
 public class DataExchange
 {
+
     private DatabaseManager dbM;
 
     public DataExchange(DatabaseManager databaseManager)
@@ -46,7 +45,7 @@ public class DataExchange
     {
         this.dbM.close();
     }
-    
+
     public static void close(ResultSet resultSet)
     {
         try
@@ -57,22 +56,48 @@ public class DataExchange
             e.printStackTrace();
         }
     }
-    
-    public void writeQuery(String q)
+
+//    public void writeQuery(String q)
+//    {
+//        this.dbM.writeQuery(q);
+//    }
+//    public void writeQuery(List<String> list)
+//    {
+//        this.dbM.writeQuery(list);
+//    }
+
+    public void writeSqlStatement(Statement s) throws SQLException
     {
-        this.dbM.writeQuery(q);
-    }
-    
-    public void writeQuery(List<String> list)
-    {
-        this.dbM.writeQuery(list);
+        s.executeBatch();
+        s.close();
     }
 
-    public DatabaseManager getDbM()
+    public Statement getSqlStatement() throws SQLException
     {
-        return dbM;
+        return this.dbM.getDbCon().createStatement();
     }
-    
+
+    public void setForiegnKeyPragma(boolean b)
+    {
+        if (b)
+        {
+            this.dbM.writeQuery("PRAGMA foreign_keys = ON;");
+        } else
+        {
+            this.dbM.writeQuery("PRAGMA foreign_keys = OFF;");
+        }
+    }
+
+    public void setAutoCommit(boolean b) throws SQLException
+    {
+        this.dbM.getDbCon().setAutoCommit(b);
+    }
+
+    public void commitNow() throws SQLException
+    {
+        this.dbM.getDbCon().commit();
+    }
+
     private int getLastID()
     {
         try
@@ -88,20 +113,21 @@ public class DataExchange
         System.exit(8);
         return -1;
     }
-    
+
     /*
         PARSERS
-    */
-    
+     */
     private List<UserEntry> parseUserEntrys(ResultSet rset)
     {
         try
         {
             UserEntry maker = new UserEntry();
-            
+
             List<UserEntry> users = new LinkedList<UserEntry>();
             while (rset.next())
+            {
                 users.add((UserEntry) maker.parseDBContent(rset));
+            }
 
             return users;
         } catch (Exception e)
@@ -110,16 +136,18 @@ public class DataExchange
             return new LinkedList<UserEntry>();
         }
     }
-    
+
     private List<ImpressionEntry> parseImpressionEntrys(ResultSet rset)
     {
         try
         {
             ImpressionEntry maker = new ImpressionEntry();
-            
+
             List<ImpressionEntry> impressions = new LinkedList<ImpressionEntry>();
             while (rset.next())
+            {
                 impressions.add((ImpressionEntry) maker.parseDBContent(rset));
+            }
 
             return impressions;
         } catch (Exception e)
@@ -128,16 +156,18 @@ public class DataExchange
             return new LinkedList<ImpressionEntry>();
         }
     }
-    
+
     private List<ClickEntry> parseClickEntrys(ResultSet rset)
     {
         try
         {
             ClickEntry maker = new ClickEntry();
-            
+
             List<ClickEntry> clicks = new LinkedList<ClickEntry>();
             while (rset.next())
+            {
                 clicks.add((ClickEntry) maker.parseDBContent(rset));
+            }
 
             return clicks;
         } catch (Exception e)
@@ -146,16 +176,18 @@ public class DataExchange
             return new LinkedList<ClickEntry>();
         }
     }
-    
+
     private List<ServerEntry> parseServerEntrys(ResultSet rset)
     {
         try
         {
             ServerEntry maker = new ServerEntry();
-            
+
             List<ServerEntry> servers = new LinkedList<ServerEntry>();
             while (rset.next())
+            {
                 servers.add((ServerEntry) maker.parseDBContent(rset));
+            }
 
             return servers;
         } catch (Exception e)
@@ -164,14 +196,16 @@ public class DataExchange
             return new LinkedList<ServerEntry>();
         }
     }
-    
+
     private Map<String, String> parseSettings(ResultSet rset)
     {
         try
         {
             Map<String, String> settings = new HashMap<String, String>();
             while (rset.next())
+            {
                 settings.put(rset.getString("name"), rset.getString("value"));
+            }
 
             return settings;
         } catch (Exception e)
@@ -180,35 +214,33 @@ public class DataExchange
             return new HashMap<String, String>();
         }
     }
-    
-    
+
     /*
         INSERT STATEMENTS
-    */
-    
+     */
     public void insertUserStmt(UserEntry user)
     {
         this.dbM.writeQuery(QueryComposer.insertUserStmt(user));
     }
-    
+
     public void insertImpressionStmt(ImpressionEntry ie)
     {
         this.dbM.writeQuery(QueryComposer.insertImpressionStmt(ie));
         ie.setId(this.getLastID());
     }
-    
+
     public void insertClickStmt(ClickEntry ce)
     {
         this.dbM.writeQuery(QueryComposer.insertClickStmt(ce));
         ce.setId(this.getLastID());
     }
-    
+
     public void insertServerStmt(ServerEntry se)
     {
         this.dbM.writeQuery(QueryComposer.insertServerStmt(se));
         se.setId(this.getLastID());
     }
-    
+
     public void insertSettingStmt(String name, String value)
     {
         this.dbM.writeQuery(QueryComposer.insertSettingStmt(name, value));
@@ -216,54 +248,48 @@ public class DataExchange
 
     /*
         UPDATE STATEMENTS
-    */
+     */
     // if you need them call me up (Marco)
-    
-    
     /*
         DELETE STATEMENTS
-    */
+     */
     // if you need them call me up (Marco)
-    
-    
     /*
         DROP ALL STATEMENTS
-    */
+     */
     public void dropAllFrom_USERS()
     {
         this.dbM.writeQuery(QueryComposer.dropAllFrom_USERS);
     }
-    
+
     public void dropAllFrom_IMPRESSION_LOGS()
     {
         this.dbM.writeQuery(QueryComposer.dropAllFrom_IMPRESSION_LOGS);
     }
-    
+
     public void dropAllFrom_CLICK_LOGS()
     {
         this.dbM.writeQuery(QueryComposer.dropAllFrom_CLICK_LOGS);
     }
-    
+
     public void dropAllFrom_SERVER_LOGS()
     {
         this.dbM.writeQuery(QueryComposer.dropAllFrom_SERVER_LOGS);
     }
-    
+
     public void dropAllFrom_SETTINGS()
     {
         this.dbM.writeQuery(QueryComposer.dropAllFrom_SETTINGS);
     }
-    
+
     public void dropAll_noSettings()
     {
         this.dbM.writeQuery(QueryComposer.dropAll_noSettings);
     }
-    
-    
+
     /*
         SELECT ALL STATEMENTS
-    */
-    
+     */
     public List<UserEntry> selectAllFrom_USERS()
     {
         ResultSet rset = this.dbM.query(QueryComposer.selectAllFrom_USERS);
@@ -271,7 +297,7 @@ public class DataExchange
         close(rset);
         return tmp;
     }
-    
+
     public List<ImpressionEntry> selectAllFrom_IMPRESSION_LOGS()
     {
         ResultSet rset = this.dbM.query(QueryComposer.selectAllFrom_IMPRESSION_LOGS);
@@ -279,7 +305,7 @@ public class DataExchange
         close(rset);
         return tmp;
     }
-    
+
     public List<ClickEntry> selectAllFrom_CLICK_LOGS()
     {
         ResultSet rset = this.dbM.query(QueryComposer.selectAllFrom_CLICK_LOGS);
@@ -287,7 +313,7 @@ public class DataExchange
         close(rset);
         return tmp;
     }
-    
+
     public List<ServerEntry> selectAllFrom_SERVER_LOGS()
     {
         ResultSet rset = this.dbM.query(QueryComposer.selectAllFrom_SERVER_LOGS);
@@ -295,7 +321,7 @@ public class DataExchange
         close(rset);
         return tmp;
     }
-    
+
     public Map<String, String> selectAllFrom_SETTINGS()
     {
         ResultSet rset = this.dbM.query(QueryComposer.selectAllFrom_SETTINGS);
@@ -303,83 +329,81 @@ public class DataExchange
         close(rset);
         return tmp;
     }
-    
+
     /*
         SELECT BY ID
-    */
-    
-    public UserEntry selectByIdFrom_USERS(String id) 
-    { 
+     */
+    public UserEntry selectByIdFrom_USERS(String id)
+    {
         ResultSet rset = this.dbM.query(QueryComposer.selectByIdFrom_USERS(id));
-        
+
         UserEntry maker = new UserEntry();
         maker = (UserEntry) maker.parseDBContent(rset);
-        
+
         close(rset);
         return maker;
     }
 
     public ImpressionEntry selectByIdFrom_IMPRESSION_LOGS(int id)
-    { 
+    {
         ResultSet rset = this.dbM.query(QueryComposer.selectByIdFrom_IMPRESSION_LOGS(id));
-        
+
         ImpressionEntry maker = new ImpressionEntry();
         maker = (ImpressionEntry) maker.parseDBContent(rset);
-        
+
         close(rset);
         return maker;
     }
-    
+
     public ClickEntry selectByIdFrom_CLICK_LOGS(int id)
-    { 
+    {
         ResultSet rset = this.dbM.query(QueryComposer.selectByIdFrom_CLICK_LOGS(id));
-        
+
         ClickEntry maker = new ClickEntry();
         maker = (ClickEntry) maker.parseDBContent(rset);
-        
+
         close(rset);
         return maker;
     }
-    
+
     public ServerEntry selectByIdFrom_SERVER_LOGS(int id)
     {
         ResultSet rset = this.dbM.query(QueryComposer.selectByIdFrom_SERVER_LOGS(id));
-        
+
         ServerEntry maker = new ServerEntry();
         maker = (ServerEntry) maker.parseDBContent(rset);
-        
+
         close(rset);
         return maker;
     }
-    
-    
+
     /*
         SELECT BY userId
-    */
+     */
     public List<ImpressionEntry> selectByUserIdFrom_IMPRESSION_LOGS(String userId)
-    { 
+    {
         ResultSet rset = this.dbM.query(QueryComposer.selectByUserIdFrom_IMPRESSION_LOGS(userId));
         List tmp = parseImpressionEntrys(rset);
         close(rset);
         return tmp;
     }
-    
+
     public List<ClickEntry> selectByUserIdFrom_CLICK_LOGS(String userId)
-    { 
+    {
         ResultSet rset = this.dbM.query(QueryComposer.selectByUserIdFrom_CLICK_LOGS(userId));
         List tmp = parseClickEntrys(rset);
         close(rset);
         return tmp;
     }
-    
+
     public List<ServerEntry> selectByUserIdFrom_SERVER_LOGS(String userId)
-    { 
+    {
         ResultSet rset = this.dbM.query(QueryComposer.selectByUserIdFrom_SERVER_LOGS(userId));
         List tmp = parseServerEntrys(rset);
         close(rset);
         return tmp;
     }
-    
+
     public String selectByNameFrom_SETTINGS(String name)
     {
         ResultSet resultSet = this.dbM.query(QueryComposer.selectByNameFrom_SETTINGS(name));

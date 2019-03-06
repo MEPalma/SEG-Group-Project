22417b8ed3d922a584d25bf773e0e91f74d18b47
mep2g17 <mep2g17@soton.ac.uk>
2019-03-06 14:45:13 +0000

    more tests...

--- Project/SEG/src/JUnitTests/JunitTestCases.java
+++ Project/SEG/src/JUnitTests/JunitTestCases.java
@@ -1,10 +1,23 @@
 package JUnitTests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import Commons.ClickEntry;
+import Commons.ImpressionEntry;
+import Commons.ServerEntry;
+import Commons.UserEntry;
+import DatabaseManager.DatabaseManager;
+import DatabaseManager.QueryComposer;
+import DatabaseManager.DataExchange;
+
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+import java.util.LinkedList;
 
 class JunitTestCases {
+
+	DataExchange dataExchange = new DataExchange(new DatabaseManager());
 
 	@Test
 	void clickLogQueryTest() {
@@ -44,6 +57,71 @@ class JunitTestCases {
 	void serverLogUserIDTest() {
 		String output = QueryComposer.selectByUserIdFrom_SERVER_LOGS("2");
 		assertEquals("SELECT * FROM SERVER_LOGS WHERE SERVER_LOGS.userId='2';", output);
+	}
+
+	void addUserContains() {
+		List<UserEntry> insertions = new LinkedList<UserEntry>();
+
+		for (int i = 0; i < 100; ++i)
+		{
+			UserEntry tmp = new UserEntry(Integer.toString(i), UserEntry.Gender.Male, UserEntry.Age.Age_25_34, UserEntry.Income.High);
+			insertions.add(tmp);
+			this.dataExchange.insertUserStmt(tmp);
+		}
+
+		List<UserEntry> result = this.dataExchange.selectAllFrom_USERS();
+
+		for (int i = 0; i < insertions.size(); ++i)
+			assertEquals(insertions.get(i).stringify(), result.get(i).stringify());
+
+	}
+
+	void addClickEntryContains() {
+		List<ClickEntry> insertions = new LinkedList<ClickEntry>();
+
+		for (int i = 0; i < 100; ++i)
+		{
+			ClickEntry tmp = new ClickEntry(ClickEntry.AUTO_INDEX, Integer.toString(i), new Date(), i);
+			insertions.add(tmp);
+			this.dataExchange.insertClickStmt(tmp);
+		}
+
+		List<ClickEntry> result = this.dataExchange.selectAllFrom_CLICK_LOGS();
+
+		for (int i = 0; i < insertions.size(); ++i)
+			assertEquals(insertions.get(i).stringify(), result.get(i).stringify());
+	}
+
+	void addImpressionEntryContains() {
+		List<ImpressionEntry> insertions = new LinkedList<ImpressionEntry>();
+
+		for (int i = 0; i < 100; ++i)
+		{
+			ImpressionEntry tmp = new ImpressionEntry(ImpressionEntry.AUTO_INDEX, Integer.toString(i), new Date(), ImpressionEntry.Context.Unknown, new Double(i));
+			insertions.add(tmp);
+			this.dataExchange.insertImpressionStmt(tmp);
+		}
+
+		List<ImpressionEntry> result = this.dataExchange.selectAllFrom_IMPRESSION_LOGS();
+
+		for (int i = 0; i < insertions.size(); ++i)
+			assertEquals(insertions.get(i).stringify(), result.get(i).stringify());
+	}
+
+	void addServerEntryContains() {
+		List<ServerEntry> insertions = new LinkedList<ServerEntry>();
+
+		for (int i = 0; i < 100; ++i)
+		{
+			ServerEntry tmp = new ServerEntry(ServerEntry.AUTO_INDEX, Integer.toString(i), new Date(), new Date(), new Double(i), ServerEntry.Conversion.Yes);
+			insertions.add(tmp);
+			this.dataExchange.insertServerStmt(tmp);
+		}
+
+		List<ServerEntry> result = this.dataExchange.selectAllFrom_SERVER_LOGS();
+
+		for (int i = 0; i < insertions.size(); ++i)
+			assertEquals(insertions.get(i).stringify(), result.get(i).stringify());
 	}
 
 }
--- Project/SEG/src/Main/Main.java
+++ Project/SEG/src/Main/Main.java
@@ -2,18 +2,14 @@ package Main;
 
 import Gui.Gui;
 import Gui.MainController;
-
 import java.util.Locale;
 
 /**
  * @author Group 31
  */
 public class Main {
-
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
         new Gui(new MainController()).setVisible(true);
-
     }
-
 }

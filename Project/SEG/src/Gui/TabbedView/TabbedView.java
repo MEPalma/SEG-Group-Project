3f1468cedd216f3c51d6eba6e28132b5df285708
alexethomas99 <alexethomas99@gmail.com>
2019-04-29 10:49:54 +0100

    removed mysterious import
    dont know where it came from

--- Project/SEG/src/Gui/TabbedView/TabbedView.java
+++ Project/SEG/src/Gui/TabbedView/TabbedView.java
@@ -9,7 +9,6 @@ import Gui.GuiComponents.TitleLabel;
 import Gui.HomeView.HomeView;
 import Gui.MainController;
 import Gui.TakeActionListener;
-//import sun.awt.image.ImageWatched;
 
 import javax.swing.*;
 import java.awt.*;
